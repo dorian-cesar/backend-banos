50667b5e7252c396cd5885712dda3340263a9816
Dorian-Cesar <dorian.celu@gmail.com>
2025-08-20 13:28:12 -0400

    deploy backend

--- server.js
+++ server.js
@@ -36,7 +36,13 @@ https.createServer(sslOptions, app).listen(PORT, () => {
   console.log(`Servidor HTTPS escuchando en https://localhost:${PORT}`);
 });
 */
-
+/*
 http.createServer(app).listen(PORT, () => {
   console.log(`Servidor HTTP escuchando en http://localhost:${PORT}`);
+});
+*/
+
+//const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
