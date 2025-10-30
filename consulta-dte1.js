6a0484c61fab0f39b50d74a0db2682f04b3f6bc6
diego <dwigodski@gmail.com>
2025-10-30 12:06:35 -0300

    fix: se elimina auth por token en controlador de boletas

--- consulta-dte1.js
+++ consulta-dte1.js
@@ -25,7 +25,7 @@ async function consultarEstadoDTE(trackId) {
         Certificado: { Rut: process.env.CERT_RUT, Password: CERT_PASS },
         RutEmpresa: `${EMISOR_RUT}-${EMISOR_DV}`,
         TrackId: trackId,
-        Ambiente: 0,
+        Ambiente: 1,
         ServidorBoletaREST: 1,
       })
     );
@@ -63,7 +63,7 @@ async function consultarEstadoDTE(trackId) {
 
 // --- Ejemplo de uso ---
 (async () => {
-  const trackId = "25446638"; // Reemplaza con tu track_id
+  const trackId = "19345102654"; // Reemplaza con tu track_id
   const estado = await consultarEstadoDTE(trackId);
   console.log(`Estado final del DTE (${trackId}):`, estado);
 })();
